c64881fa1babd1e4e396a4e7df427591490fde82
sergeiiak <sergeiiak@yandex-team.ru>
2018-08-27 22:49:24 +0100

    first commit

---
+++ src/bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+const program = require('commander');
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'Output format')
+  .parse(process.argv);
